785a216762a798a6bec0a2b23ed43ea2318b6d0b
Adlan Uzair bin Amer Hafizi <118761084+adlan2001@users.noreply.github.com>
2024-12-07 18:47:30 +0800

    Added team profiles

---
+++ teamProfile.js
@@ -0,0 +1,21 @@
+var teamProfile = [
+  {
+    photo: "guy.jpg",
+    name: "Peter Griffin",
+    role: "Programmer",
+    linkedin: "https://linkedin.com",
+    twitter: "https://x.com"
+  },
+  {
+    photo: "guy.jpg",
+    name: "Peter Griffin",
+    role: "Programmer",
+    linkedin: "https://linkedin.com"
+  },
+  {
+    photo: "guy.jpg",
+    name: "Peter Griffin",
+    role: "Programmer",
+    github: "https://github.com"
+  }
+];
